cd4ab259e9dd6609030cc647b32eb42393b79780
Ilaria Rosati <llamavida@gmail.com>
2024-01-22 16:09:11 +0100

    U1-W2-D1

---
+++ RecipeJS.js
@@ -0,0 +1,38 @@
+let ricetta = "Tortino di cioccolato con cuore fondente";
+console.log(ricetta);
+
+let cioccolato = "Cioccolato fondente al 70%";
+console.log(cioccolato);
+let cioccolato1 = 20;
+let cioccolato2 = 90;
+let cioccolato3 = "g";
+console.log(cioccolato1 + cioccolato2 + cioccolato3);
+
+let uova = "Uova (2 grandi)";
+console.log(uova);
+let uova1 = 2;
+let uova2 = 60;
+let uova3 = "g";
+console.log(uova1 * uova2 + uova3);
+
+let farina = "Farina 00";
+console.log(farina);
+let farina1 = 27;
+let farina2 = 10;
+let farina3 = 12;
+let farina4 = "g";
+console.log(farina1 + farina2 - farina3 + farina4);
+
+let burro = "Burro temperatura ambiente";
+console.log(burro);
+let burro1 = 110;
+let burro2 = 1;
+let burro3 = "g";
+console.log(burro1 * burro2 + burro3);
+
+let zucchero = "Zucchero";
+console.log(zucchero);
+let zucchero1 = 120;
+let zucchero2 = 3;
+let zucchero3 = "g";
+console.log(zucchero1 / zucchero2 + zucchero3);
